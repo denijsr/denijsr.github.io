16765af42d1fc92cce273bd79bfa203fabcbea3a
denijsr <denijs.krauklis@gmail.com>
2022-05-13 02:00:49 +0300

    Added new files

---
+++ 1.js
@@ -0,0 +1,43 @@
+words1 = ['ab4','cb4','cd4','ed4','ef4','gf4','gh4']
+
+function random1() {
+  document.getElementById("1").value = words1[Math.floor(Math.random() * words1.length)];
+}
+
+function random12() {
+    document.getElementById("2").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random13() {
+    document.getElementById("3").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random14() {
+    document.getElementById("4").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random15() {
+    document.getElementById("5").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random16() {
+    document.getElementById("6").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random17() {
+    document.getElementById("7").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random18() {
+    document.getElementById("8").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random19() {
+    document.getElementById("9").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+  function random110() {
+    document.getElementById("10").value = words1[Math.floor(Math.random() * words1.length)];
+  }
+
+console.log("Everts lohs");
---
+++ 150.js
@@ -0,0 +1,56 @@
+words150 = ['cd4 ba5 dc5',	'ef4 dc5 fe3',	'gh4 hg5',	'gf4 de5',	'ed4 fg5 fe3',	'cb4 de5',	'gh4 fg5',	'gf4 bc5 cd4',	'ab4 hg5 ba5',	'gh4 hg5 cb4',
+'gh4 ba5 hg3',	'cd4 hg5 gh4',	'ef4 fg5 cb4',	'ef4 fe5 ab4',	'ab4 fe5 ba5',	'gf4 bc5',	'ab4 hg5 gf4',	'ed4 fe5',	'gh4 dc5 cb4',	'gf4 ba5 cd4',
+'cd4 fg5 dc5',	'gh4 dc5 fg3',	'gf4 fe5 hg3',	'cb4 hg5',	'ab4 de5',	'gh4 de5 cb4',	'ef4 ba5',	'cb4 fg5',	'gh4 bc5 fg3',	'ef4 dc5 de3',
+'cb4 fe5 dc3',	'gh4 bc5 cd4',	'ab4 fg5 bc5',	'cd4 dc5 gh4',	'ab4 fe5 gh4',	'gh4 ba5',	'ab4 fe5 bc5',	'ef4 fe5 de3',	'ab4 ba5 gh4',	'gh4 de5 fg3',
+'gf4 fg5',	'gh4 bc5',	'ab4 ba5 ba3',	'ab4 de5 ba5',	'cd4 hg5',	'cd4 fg5',	'Free',	'gh4 hg5 fg3',	'ab4 fg5 ba3',	'gh4 ba5 fg3',
+'cb4 bc5',	'gf4 ba5 hg3',	'cb4 ba5 dc3',	'ed4 fg5',	'ef4 dc5 cb4',	'cd4 de5 bc3',	'ef4 fg5',	'ab4 de5 gh4',	'ed4 de5 ab4',	'cb4 fe5 bc3',
+'cd4 fg5 bc3',	'cd4 fe5',	'cd4 hg5 bc3',	'ef4 bc5',	'gh4',	'cb4 dc5',	'cb4 fe5 bc5',	'ed4 fg5 de3',	'ed4 hg5',	'ed4 dc5 fe3',
+'ab4 fg5',	'cd4 de5',	'cb4 fg5 dc3',	'ef4 fe5 fe3',	'ed4 dc5 de3',	'ef4 bc5 de3',	'ed4 bc5',	'cd4 dc5',	'cb4 bc5 ba5',	'gf4 fg5 cb4',
+'ab4 fe5',	'ef4 fe5 cb4',	'ab4 fg5 ba5',	'cb4 bc5 dc3',	'ed4 dc5',	'ab4 bc5 ba5',	'gf4 fe5',	'gf4 bc5 fg3',	'gh4 dc5',	'gf4 bc5 hg3',
+'cd4 ba5',	'gf4',	'cb4 fe5',	'cd4 de5 gf4',	'ed4 hg5 gh4',	'gf4 ba5',	'cd4 hg5 gf4',	'cb4 de5 ef4',	'gh4 bc5 cb4',	'cd4 bc5',
+'ef4 ba5 fe3',	'ef4 de5',	'ed4 ba5 ab4',	'cb4 hg5 ba5',	'ab4 hg5 ba3',	'ab4 hg5 gh4',	'ed4 ba5 dc5',	'ab4 ba5',	'ef4 ba5 cd4',	'ef4',
+'gh4 fe5 ed4',	'gf4 ba5 cb4',	'ab4 hg5',	'ab4 bc5',	'cd4',	'cb4 fg5 gh4',	'gh4 dc5 hg3',	'cd4 ba5 bc3',	'cb4 de5 dc3',	'ab4 hg5 bc5',
+'ed4',	'ef4 bc5 cb4',	'gf4 dc5',	'gf4 fg5 cd4',	'ef4 fe5',	'cb4 fe5 ba5',	'ed4 ba5',	'cb4 ba5',	'gh4 hg5 hg3',	'gh4 de5 hg3',
+'gf4 dc5 cb4',	'ab4',	'cd4 de5 dc3',	'ed4 fg5 cb4',	'cb4 de5 bc3',	'gf4 dc5 cd4',	'ed4 de5',	'gh4 bc5 hg3',	'ef4 dc5',	'gf4 bc5 cb4',
+'ab4 dc5',	'gh4 fe5',	'cb4 de5 ba5',	'ed4 fg5 gh4',	'gh4 de5',	'ed4 de5 gh4',	'cb4',	'cb4 bc5 bc3',	'cb4 hg5 gf4',	'gh4 fe5 cb4'
+]
+
+function random150() {
+  document.getElementById("150").value = words150[Math.floor(Math.random() * words150.length)];
+}
+
+function random1502() {
+    document.getElementById("2").value = words150[Math.floor(Math.random() * words150.length)];
+  }
+
+  function random1503() {
+    document.getElementById("3").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
+
+  function random1504() {
+    document.getElementById("4").value = words150[Math.floor(Math.random() * words150.length)];
+  }
+
+  function random1505() {
+    document.getElementById("5").value = words150[Math.floor(Math.random() * words150.length)];
+  } 
+
+  function random1506() {
+    document.getElementById("6").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
+
+  function random1507() {
+    document.getElementById("7").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
+  
+  function random1508() {
+    document.getElementById("8").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
+  
+  function random1509() {
+    document.getElementById("9").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
+
+  function random15010() {
+    document.getElementById("10").value = words150[Math.floor(Math.random() * words150.length)];
+  }  
